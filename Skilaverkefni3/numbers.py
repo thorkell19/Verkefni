955308dcf12bc1cd019f920ead344dd6881666e1
Thorkell <thorkell19@ru.is>
2019-09-11 19:23:52 +0000

    Done

--- Skilaverkefni3/numbers.py
+++ Skilaverkefni3/numbers.py
@@ -1,19 +1,24 @@
-#PURPOSE 1 jákvæðar tölur n frá 10-99 (formið ab)
-#þar sem (a+b)^2 = n
+#PURPOSE 1: Sum of digits squared equals number.
+
+for n in range(10,100):
+    #split n into first and second digit.
+    first = n // 10
+    second = n % 10
+    sum_dig_sq = (first + second)**2
+    if sum_dig_sq == n:
+        print(n)
 
 
-
-#PURPOSE 2: Divisor counting
+#PURPOSE 2: Ten divisors.
 count_div = 0
 
-for i in range(1,100):
-    n = i
-    #Count how many divisors each number n has.
-    for j in range(1,n+1):
-        if n % j == 0:
+for m in range(1,100):
+    #count how many divisors m has.
+    for i in range(1,m+1):
+        if m % i == 0:
             count_div += 1
-    #For each number n, check if it has 10 divisors. 
+    #check if m has 10 divisors.
     else:
         if count_div == 10:
-            print(n)
+            print(m)
         count_div = 0
