9659b755ed8c50c11dd172386891924bad1cb0f9
Thorkell <thorkell19@ru.is>
2019-10-08 13:05:02 +0000

    First commit

--- Skilaverkefni4/move.py
+++ Skilaverkefni4/move.py
@@ -1,43 +1,44 @@
-# Athugið eftirfarandi:
-# Tölurnar 1 og 10 sem afmarka bilið eiga að vera útfærðar með föstum. Fasti í Python er breyta hvers gildi á ekki að breytast í forriti. 
-# Góð regla er að nota hástafi fyrir nöfn á föstum. Þið eigið að nota föll í útfærslunni á verkefninu. 
-# Það er í lagi að aðalforritið ykkar innihaldi lykkjuna en meginmál lykkjunnar á að mestu að vera framkvæmt með fallaköllum.
+#constants for setting range
+MIN = 1
+MAX = 10
 
-pos = int(input("Input a position between 1 and 10: "))
-tenx = "xxxxxxxxxx"
-
-if 1 <= pos <= 10:
-    for index, letter in enumerate(tenx):
-        if index != pos-1:
-            print(letter, end="")
-        else:
-            print("o", end="")
-    print()
-    print("l - for moving left")
-    print("r - for moving right")
-    print("Any other letter for quitting")
-
-    move = input("Input your choice: ")
-    if move == "l" and pos > 1:
-        pos -= 1
-    if move == "r" and pos < 10:
-        pos += 1
-
-    while move == "l" or move == "r":
-        for index, letter in enumerate(tenx):
-            if index != pos-1:
-                print(letter, end="")
+def posi(pos):
+#function for graphing current position of character
+    seq = ""
+    if MIN <= pos <= MAX:
+        for i in range(MIN, MAX+1):
+            if i != pos:
+                seq += "x"
             else:
-                print("o", end="")
-        print()
-        move = input("Input your choice: ")
-        if move == "l" and pos > 1:
-            pos -= 1
-        if move == "r" and pos < 10:
-            pos += 1
+                seq += "o"
+    return seq
+
+def move(next):
+#function for moving character
+    if next == "l" and pos > 1:
+        return -1
+    elif next == "r" and pos < 10:
+        return 1
     else:
-        for index, letter in enumerate(tenx):
-            if index != pos-1:
-                print(letter, end="")
-            else:
-                print("o", end="")
+        return 0
+
+#receive and print initial position
+pos = int(input("Input a position between 1 and 10: "))
+print(posi(pos))
+
+#explain rules
+print("l - for moving left")
+print("r - for moving right")
+print("Any other letter for quitting")
+
+#receive second position befor entering loop
+next = input("Input your choice: ")
+
+#keep receiving and printing next position until input is invalid
+while next == "l" or next == "r":
+    pos += move(next)
+    print(posi(pos))
+    next = input("Input your choice: ")
+else:
+    print(posi(pos))
+
